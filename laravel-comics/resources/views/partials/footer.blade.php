b8b81e7040f6b6ed3848746ff18aa19af47cf08f
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-16 20:40:27 +0200

    feat: add footer

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,13 +1,63 @@
 <footer class="bg-white">
     <div class="container">
-        <ul>
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#">6</a></li>
-
+        <div class="d-flex justify-content-between">
+        <ul class="my-5 d-flex gap-5">
+            <div class=" text-decoration-none">
+                <h4 class="text-white fw-bold mb-3">DC COMICS</h4>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+                <h4 class="text-white fw-bold my-3">SHOP</h4>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+            </div>
+            <div>
+                <h4 class="text-white fw-bold mb-3">DC</h4>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent WorkShops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </div>
+            <div>
+                <h4 class="text-white fw-bold mb-3">SITES</h4>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+            </div>
         </ul>
+        <img class="logo" src="{{Vite::asset("resources/img/dc-logo-bg.png")}}" alt="logo grande dc">
     </div>
+    </div>
+    <section class="footer-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <a class="sign-up" href="#">SIGN-UP NOW!</a>
+                <div class="d-flex align-items-center ">
+                    <h4>FOLLOW US</h4>
+                    <ul class="d-flex gap-4 m-0">
+                        <li><a href="#"><img src="{{Vite::asset("resources/img/footer-facebook.png")}}" alt="icona facebook"></a></li>
+                        <li><a href="#"><img src="{{Vite::asset("resources/img/footer-twitter.png")}}" alt="icona twitter"></a></li>
+                        <li><a href="#"><img src="{{Vite::asset("resources/img/footer-youtube.png")}}" alt="icona youtube"></a></li>
+                        <li><a href="#"><img src="{{Vite::asset("resources/img/footer-pinterest.png")}}" alt="icona pinterest"></a></li>
+                        <li><a href="#"><img src="{{Vite::asset("resources/img/footer-periscope.png")}}" alt="icona periscope"></a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </footer>
